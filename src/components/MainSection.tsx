11b4f95b19ed386baa0f7e49ff51efa21f04eaaa
zubayrlatief <zubayrlatie007@gmail>
2025-05-21 00:13:30 +0200

    video

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -3,7 +3,18 @@ import React from 'react'
 export const MainSection: React.FC = () => {
   return (
     <div className="main-section">
-      <h2>Main Section</h2>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="background-video"
+      >
+        <source src="https://zubayrlatief.github.io/webwiser-hosted-images/desktop_video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content-overlay">
+      </div>
     </div>
   )
 }
